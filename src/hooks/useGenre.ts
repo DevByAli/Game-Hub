cf88437b17a4ee1b4bb78bcaeffa52e32377acac
DevByAli <143414566+DevByAli@users.noreply.github.com>
2023-09-06 12:40:52 +0500

    fetch genres by using react query

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,4 +1,7 @@
 // import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 import genre from "../data/genre";
 
 export interface Genre {
@@ -9,5 +12,15 @@ export interface Genre {
 
 // const useGenre = () => useData<Genre>("/genres");
 
-const useGenre = () => ({ data: genre, error: null, isLoading: false });
+// const useGenre = () => ({ data: genre, error: null, isLoading: false });
+
+const useGenre = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+    initialData: { count: genre.length, results: genre },
+  });
+
 export default useGenre;

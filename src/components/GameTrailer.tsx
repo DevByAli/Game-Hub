c4d6f868cc0fcbbc2494592e0d1db935470457fd
DevByAli <143414566+DevByAli@users.noreply.github.com>
2023-09-11 10:08:16 +0500

    final

--- src/components/GameTrailer.tsx
+++ src/components/GameTrailer.tsx
@@ -13,7 +13,7 @@ const GameTrailer = ({ gameId }: Prop) => {
   const first = data?.results[0];
 
   return first ? (
-    <video width="100%" src={first.data[480]} poster={first.preview} controls/>
+    <video width="100%" src={first.data.max} poster={first.preview} controls />
   ) : null;
 };
 export default GameTrailer;
